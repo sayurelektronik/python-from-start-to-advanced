ef72d01bfcf26f1dc84d120b2f292e864f7a7b06
sayurelektronik <sayurelektronik@gmail.com>
2024-09-08 17:37:09 +0700

    Perulangan

---
+++ Perulangan/main.py
@@ -0,0 +1,16 @@
+# For Loop: Digunakan untuk mengulangi sesuatu beberapa kali.
+# While Loop: Mengulangi selama kondisi tertentu terpenuhi.
+
+
+# Contoh For Loop:
+for i in range(5):
+    print("Angka:", i)
+
+print("")    
+# Contoh While Loop:
+angka = 0
+while angka < 5:
+    print("Angka:", angka)
+    angka += 1
+
+

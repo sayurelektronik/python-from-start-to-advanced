83fb7927fdbceb53ff97122e0c11da2477b9ee8a
sayurelektronik <sayurelektronik@gmail.com>
2024-09-08 20:05:07 +0700

    tuple

---
+++ 9. Tuple/main.py
@@ -0,0 +1,8 @@
+# Tuple mirip dengan list, tetapi elemen-elemen di dalam tuple tidak bisa diubah (immutable).
+
+# Contoh:
+# Membuat tuple
+angka = (1, 2, 3, 4)
+
+# Akses elemen tuple
+print(angka[1])  # Output: 2

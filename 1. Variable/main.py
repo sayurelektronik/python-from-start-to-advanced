2e850edafe8b1562ee356b4f4b176ab00b590da4
sayurelektronik <sayurelektronik@gmail.com>
2024-09-08 18:39:43 +0700

    update variable

---
+++ 1. Variable/main.py
@@ -0,0 +1,13 @@
+# Variabel adalah tempat untuk menyimpan nilai. Di Python, kamu tidak perlu menyebutkan tipe data saat mendeklarasikan variabel (Python secara otomatis menentukan tipe data).
+# Contoh penggunaan variabel:
+
+# Deklarasi variable
+nama = "Ardiansyah"
+umur = 27
+berat_badan = 54.5
+
+
+# Output
+print("Nama:", nama)
+print("Umur:", umur)
+print("Berat Badan:", berat_badan)

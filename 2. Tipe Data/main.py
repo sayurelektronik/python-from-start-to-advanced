f9a3f6f2a48c4492bbd205cdff8cee54eb2c9fc5
sayurelektronik <sayurelektronik@gmail.com>
2024-09-08 18:41:08 +0700

    update tipe data

---
+++ 2. Tipe Data/main.py
@@ -0,0 +1,23 @@
+# Python memiliki beberapa tipe data dasar:
+
+# Integer (int): Bilangan bulat, seperti 1, 100, -5.
+# Float (float): Bilangan desimal, seperti 3.14, 0.5.
+# String (str): Teks, seperti "Halo", "Python".
+# Boolean (bool): Nilai True atau False.
+# Contoh tipe data:
+
+# Integer
+a = 10
+print(type(a))  # Output: <class 'int'>
+
+# Float
+b = 3.14
+print(type(b))  # Output: <class 'float'>
+
+# String
+c = "Halo Dunia"
+print(type(c))  # Output: <class 'str'>
+
+# Boolean
+d = True
+print(type(d))  # Output: <class 'bool'>

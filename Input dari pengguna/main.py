2a37246dd671d89718c7288bf8d4faa22178f186
sayurelektronik <sayurelektronik@gmail.com>
2024-09-08 18:09:56 +0700

    input dari pengguna

---
+++ Input dari pengguna/main.py
@@ -0,0 +1,12 @@
+# Kamu bisa meminta input dari pengguna menggunakan fungsi input().
+
+# Contoh
+nama = input("Masukkan nama kamu: ")
+print("Halo,", nama)
+
+
+# Python selalu menganggap input sebagai string, jadi kamu mungkin perlu mengonversinya ke tipe data lain seperti integer atau float.
+# Contoh konversi input:
+umur = int(input("Masukkan umur kamu: "))
+print("Umur kamu adalah", umur)
+
